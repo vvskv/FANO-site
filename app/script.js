a5f5056744b12f6fe0229ff32bb43c8eaca97bb2
vvskv <116917067+vvskv@users.noreply.github.com>
2023-01-17 10:58:17 +0300

    create about

---
+++ app/script.js
@@ -0,0 +1,1 @@
+https://api.vk.com/api.php?oauth=1&amp;method=wall.get&amp;owner_id=-25334606&amp;count=50&amp;v=5.67&amp;access_token=d967fbe5d967fbe5d967fbe541da75bb04dd967d967fbe5baa1a9e3b982da084134f66d
