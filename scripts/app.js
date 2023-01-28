f49084a6d8f0b26b29e2aacdc3c9b10e8c75af55
vvskv <vvskvorcov@yandex.ru>
2023-01-28 14:49:10 +0300

    menu create

--- scripts/app.js
+++ scripts/app.js
@@ -1,81 +1,109 @@
-;(function() {
-	'use strict'
+(function () {
+      "use strict";
 
-	class ScrollBox {
+      class ScrollBox {
+            constructor(container) {
+                  this.scrollBox = container;
+                  this.init();
+            }
 
-		constructor(container) {
-			this.scrollBox = container;
-			this.init();
-		};
-		
-		init() {
-			this.scrollBox.addEventListener('mouseover', ()=> {
-				document.documentElement.style.setProperty("--scroller-color", '#a3b3c9');
-			});
-			this.scrollBox.addEventListener('mouseout', ()=> {
-				document.documentElement.style.setProperty("--scroller-color", 'inherit');
-			})
-		}
+            init() {
+                  this.scrollBox.addEventListener("mouseover", () => {
+                        document.documentElement.style.setProperty(
+                              "--scroller-color",
+                              "#a3b3c9"
+                        );
+                  });
+                  this.scrollBox.addEventListener("mouseout", () => {
+                        document.documentElement.style.setProperty(
+                              "--scroller-color",
+                              "inherit"
+                        );
+                  });
+            }
+      }
 
-	}
-
-	const scrollBoxes = document.querySelectorAll('.scroll-box');
-	for (let box of scrollBoxes) {
-		const scrollBox = new ScrollBox(box);
-	}
+      const scrollBoxes = document.querySelectorAll(".scroll-box");
+      for (let box of scrollBoxes) {
+            const scrollBox = new ScrollBox(box);
+      }
 })();
 
-;(function() {
-	'use strict'
-	class OpenedMenu {
-		
-		constructor(menu) {
-			this.openedMenu = menu.querySelector('.submenu__link');
-			this.subMenu = menu.querySelector('.submenu__wrap');
-			this.hamburgerCheck = document.querySelector('.hamburger-wrap');
-			this.screenWidth = document.body.clientWidth;
-			this.clickToggle = this.clickToggle.bind(this);
-			this.show = this.show.bind(this);
-			this.hide = this.hide.bind(this);
-			this.init();
-		};
+(function () {
+      "use strict";
+      class OpenedMenu {
+            constructor(menu) {
+                  this.openedMenu = menu.querySelector(".submenu__link");
+                  this.subMenu = menu.querySelector(".submenu__wrap");
+                  this.hamburgerCheck =
+                        document.querySelector(".hamburger-wrap");
+                  this.clickToggle = this.clickToggle.bind(this);
+                  this.show = this.show.bind(this);
+                  this.hide = this.hide.bind(this);
+                  this.init();
+            }
 
-		init() {
-			if (getComputedStyle(this.hamburgerCheck).display != 'flex') {
-				// console.log('111');
-				this.subMenu.addEventListener('mouseover', this.show);
-				this.subMenu.addEventListener('mouseout', this.hide);	
-				this.openedMenu.addEventListener('click', (e)=> {e.preventDefault()});
-				this.openedMenu.addEventListener('mouseover', this.show);
-				this.openedMenu.addEventListener('mouseout', this.hide);
-			} else {
-				this.openedMenu.addEventListener('click', this.clickToggle);
-			}			
-			
-		}
-		clickToggle(e) {
-			e.preventDefault();
-			const openSubMenuArr = document.querySelectorAll('.submenu__wrap');
-				for (let item of openSubMenuArr) {
-					item.classList.remove('submenu__wrap--show');
-				}			 
-			this.subMenu.classList.toggle('submenu__wrap--show');
-		}
-		show() {
-			this.subMenu.classList.add('submenu__wrap--show');
-		}
-		hide() {
-			this.subMenu.classList.remove('submenu__wrap--show');
-		}
-		resetToggle() {
-			
-		}
-	}
+            init() {
+                  if (getComputedStyle(this.hamburgerCheck).display != "flex") {
+                        // console.log('111');
+                        this.subMenu.addEventListener("mouseover", this.show);
+                        this.subMenu.addEventListener("mouseout", this.hide);
+                        this.openedMenu.addEventListener("click", (e) => {
+                              e.preventDefault();
+                        });
+                        this.openedMenu.addEventListener(
+                              "mouseover",
+                              this.show
+                        );
+                        this.openedMenu.addEventListener("mouseout", this.hide);
+                  } else {
+                        this.openedMenu.addEventListener(
+                              "click",
+                              this.clickToggle
+                        );
+                  }
+            }
+            clickToggle(e) {
+                  e.preventDefault();
+                  const subMenuShowCheck = this.subMenu.classList.contains(
+                        "submenu__wrap--show"
+                  );
+                  if (!subMenuShowCheck) {
+                        // Проверка на открытые пункты меню и их закрытие
+                        const openSubMenuArr =
+                              document.querySelectorAll(".submenu__wrap");
+                        for (let item of openSubMenuArr) {
+                              item.classList.remove("submenu__wrap--show");
+                        }
+                        this.subMenu.classList.add("submenu__wrap--show");
+                  } else {
+                        this.subMenu.classList.remove("submenu__wrap--show");
+                  }
+            }
+            show() {
+                  this.subMenu.classList.add("submenu__wrap--show");
+            }
+            hide() {
+                  this.subMenu.classList.remove("submenu__wrap--show");
+            }
+            resetToggle() {}
+      }
 
-	const allOpenedMenu = document.querySelectorAll('.submenu');
-	for (let menu of allOpenedMenu) {
-		const openedMenu = new OpenedMenu(menu);
-	}
+      const allOpenedMenu = document.querySelectorAll(".submenu");
+      for (let menu of allOpenedMenu) {
+            const openedMenu = new OpenedMenu(menu);
+      }
 })();
 
-
+(function () {
+      "use strict";
+	const hamburger = document.querySelector('#hamburger-toggle');
+	const allSubMenu = document.querySelectorAll('.submenu__wrap');
+	hamburger.addEventListener('change', ()=> {
+		if (!hamburger.checked) {
+			for (let item of allSubMenu) {
+				item.classList.remove('submenu__wrap--show');
+			}
+		} 
+	})
+})();
