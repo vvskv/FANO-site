47931763812f7ab26e65256f58a512e7a98b5ac1
vvskv <vvskvorcov@yandex.ru>
2023-02-12 09:39:44 +0300

    correct

--- scripts/loadtable.js
+++ scripts/loadtable.js
@@ -3,34 +3,36 @@
       const table = document.querySelector(".sp-table");
       const warehouse = document.querySelector(".table-warehouse").children;
       const media1100 = window.matchMedia("(max-width: 1100px)");
-      const mediaMain = window.matchMedia("(min-width: 1101px)");
+      const media650 = window.matchMedia("(max-width: 650px)");
       let numberOfColumns = 3;
-      // let arrItems = warehouse;
 
-      // for(let item of warehouse) {
-      //       arrItems.push(item);
-      // }
-
-      if (table) fillingColumn(numberOfColumns);
-
-      // media1100.addListener( () => {
-      //       console.log("!!!!!!!");
-      //       numberOfColumns = 2;
-      //       fillingColumn(2);
-      // });
-      mediaMain.addEventListener("change", ()=> {
-            // fillingColumn(3);
-      })
-      media1100.addEventListener("change", ()=> {
-            // console.log(window.screen.width);
-            if(window.innerWidth > 1100) {
-                  fillingColumn(3)
-            } else {
+      // if (table) fillingColumn(numberOfColumns);
+      if (window.innerWidth > 1100) {
+            fillingColumn(3);
+      } else if (window.innerWidth > 650) {
             fillingColumn(2);
+      } else {
+            fillingColumn(1);
+      };
 
+      media1100.addEventListener("change", () => {
+            console.log("!!!");
+            if (window.innerWidth > 1100) {
+                  fillingColumn(3);
+            } else if (window.innerWidth < 1100) {
+                  fillingColumn(2);
             }
       });
-      function fillingColumn(n) {
+
+      media650.addEventListener("change", () => {
+            if (window.innerWidth > 650) {
+                  fillingColumn(2);
+            } else {
+                  fillingColumn(1);
+            }
+      });
+
+      function fillingColumn(n, m) {
             table.innerHTML = "";
             for (let index = 0; index < n; index++) {
                   createColumn(n);
@@ -42,40 +44,15 @@
                   if (index % n === 0 && index != 0) {
                         temp += n;
                   }
-                  tableColumn[index - temp].append(warehouse[index].cloneNode(true));
+                  tableColumn[index - temp].append(
+                        warehouse[index].cloneNode(true)
+                  );
             }
       }
       function createColumn(n) {
             let column = document.createElement("div");
             column.classList.add("sp-table__column");
-            column.style.width = `calc(${100/n}% - ${50*(n-1)/n}px)`;
+            column.style.width = `calc(${100 / n}% - ${(50 * (n - 1)) / n}px)`;
             table.append(column);
       }
-
-      // function fillingColumn(n) {
-      //       table.innerHTML = "";
-      //       for (let index = 0; index < n; index++) {
-      //             createColumn(n);
-      //       }
-      //       const tableColumns = table.children;
-      //       const warehouseLength = warehouse.length;
-
-      //       for (let index = 0; index < warehouseLength; index++) {
-      //             const element = array[index];
-                  
-      //       }
-
-      // }
-
-      // class TableItem {
-      //       constructor(index, content) {
-      //             this.index = index;
-      //             this.content = content;
-      //       }
-      // }
-      // for (let index = 0; index < warehouse.length; index++) {
-      //       const element = new TableItem(index, warehouse[index]);
-      //       arrItems.push(element);
-      // }
-      
 })();
