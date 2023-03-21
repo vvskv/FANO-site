c5d999148f41d808ff498155d6f8c8a6be4c30d9
vvskv <116917067+vvskv@users.noreply.github.com>
2023-03-21 14:37:33 +0300

    vk news table

--- scripts/loadtable.js
+++ scripts/loadtable.js
@@ -6,9 +6,12 @@
       const media650 = window.matchMedia("(max-width: 650px)");
       // получение массива с размера картинок
       const strHeightImg = document.querySelector(".data-php").getAttribute("data-attr");
-      const arrHeightImg = JSON.parse(strHeightImg);
+      let arrHeightImg = JSON.parse(strHeightImg);
+      arrHeightImg.unshift(0);
+      arrHeightImg.push(0);
 
-      // console.log(arrHeightImg[0]);
+      // console.log(arrHeightImg);
+      // console.log(warehouse);
 
       if (window.innerWidth > 1100) {
             fillingColumn(3);
@@ -40,45 +43,55 @@
                   createColumn(n);
             }
             const tableColumns = table.children;
+
+            let tableColumnsMap = [...tableColumns].map(function(column) {
+                  return {
+                        size: 0,
+                        column: column,
+                  }
+            })
+
             const warehouseLength = warehouse.length;
             for (let index = 0; index < warehouseLength; index++) {
-                  if(table.classList.contains("mp-news__table") && index > 15) {
-                        // console.log(index);
-                        let minColHeight = getMinColumn(tableColumns).scrollHeight;
-                        let maxColHeight = getMaxColumn(tableColumns).scrollHeight;
-                        warehouse[index].style.display = 'flex';
-                        let itemHeight = warehouse[index].offsetHeight;
+                  let curCol = getMinColumn(tableColumnsMap); //Колонка минимального размера
+                  // let item = warehouse[index].cloneNode(true);
+                  // curCol.column.append(warehouse[index].cloneNode(true));
+                  // curCol.column.append(item.style.display = "flex");
 
-                        warehouse[index].style.display = 'none';
-                        
-                        if ((itemHeight) > (maxColHeight - minColHeight)) {
-                              break;
-                        } else {
-                              getMinColumn(tableColumns).append(warehouse[index].cloneNode(true));
-                        }
-                  } else {
-                        getMinColumn(tableColumns).append(warehouse[index].cloneNode(true));
+
+                  // warehouse[index].style.display = 'flex';
+                  let itemHeight = warehouse[index].offsetHeight; //Размер добавляемого поста (без изображения)
+                  // warehouse[index].style.display = 'none';
+                  curCol.column.append(warehouse[index].cloneNode(true));
+
+                  console.log(itemHeight);
+
+                  const itemHeightAll = itemHeight + arrHeightImg[index]; // Полный размер поста
+                  curCol.size += itemHeightAll;
+                  if(index > 10 && table.classList.contains("mp-news__table")) {
+                        const maxColSize = getMaxColumn(tableColumnsMap).size;
+                        const minColSize = getMinColumn(tableColumnsMap).size;
+                        if (maxColSize - minColSize + 150< itemHeightAll) break;
                   }
             }
       }
 
       function getMinColumn(arr) {
             let result = arr[0];
-            for (let i = 1; i < arr.length; i++) {
-                  
-                  if(arr[i].offsetHeight < result.offsetHeight) {
-                        result = arr[i];
-                  }                   
-            }            
+            arr.forEach(element => {
+                  if(element.size < result.size ) {
+                        result = element;
+                  }
+            });
             return result;
       }
       function getMaxColumn(arr) {
             let result = arr[0];
-            for (let i = 1; i < arr.length; i++) {
-                  if(arr[i].offsetHeight > result.offsetHeight) {
-                        result = arr[i];
-                  }                   
-            }            
+            arr.forEach(element => {
+                  if(element.size > result.size ) {
+                        result = element;
+                  }
+            });
             return result;
       }
       function createColumn(n) {
